af3371dee78ff725837853d8e18f3676f17878e4
VitalikL <lim.vitaliy@gmail.com>
2017-08-07 16:18:49 -0400

    Adding twitter library and twitter text chack to make twitter profile optional

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var path = require("path"),
     request = require("request"),
-    fs = require("fs");
+    fs = require("fs"),
+    twitter = require("twitter-text");
 
 MEDIA_EXTENSION_PNG = ".png";
 MEDIA_EXTENSION_JPG = ".jpg";
@@ -19,9 +20,15 @@ var list_posts = function(file) {
         text = fs.readFileSync(textFile)
     }
 
-    var media = encodeURIComponent("https://raw.githubusercontent.com/Adventech/adventech-social/master/"+file.replace(" ", "%20"));
+    var media = encodeURIComponent("https://raw.githubusercontent.com/Adventech/adventech-social/master/"+file.replace(" ", "%20")),
+        twitterProfile = '';
 
-    request.post("https://api.bufferapp.com/1/updates/create.json?access_token=" + TOKEN).form("text="+text+"&profile_ids%5B%5D=586a761340b8c63a719b2c22&profile_ids%5B%5D=586a77b1b6684f861d9b2bfd&profile_ids%5B%5D=586a77ecb6684f0f219b2bf7&media%5Bphoto%5D=" + media)
+
+    if (twitter.getTweetLength(text) <= 140){
+        twitterProfile = "&profile_ids%5B%5D=586a77ecb6684f0f219b2bf7";
+    }
+
+    request.post("https://api.bufferapp.com/1/updates/create.json?access_token=" + TOKEN).form("text="+text+"&profile_ids%5B%5D=586a761340b8c63a719b2c22&profile_ids%5B%5D=586a77b1b6684f861d9b2bfd"+twitterProfile+"&media%5Bphoto%5D=" + media)
 };
 
 process.argv.forEach(function (val, index, array) {
